ead78c8e7ab5912640dafd9094873fe2a7da6f2e
Michael-Cullen-SKC <95493660+Mcullen22@users.noreply.github.com>
2022-01-12 22:51:55 +0000

    Add files via upload

---
+++ Week 3 ending 240921/Random Number 220921.py
@@ -0,0 +1,14 @@
+#import random
+#list = ('happy','sad')
+#result = random.choice(list)
+#print(result)
+
+import random
+
+low = random.randint(1,10)
+high = random.randint(low,10)
+
+#print (low)
+#print (high)
+
+print('The low number is', low, '\nAnd the high number is',high)
